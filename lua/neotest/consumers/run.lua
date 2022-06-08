44e783b2b5e3421fa58af64252bf7bdcfa855a3b
Rónán Carrigan <rcarriga@tcd.ie>
2022-06-08 08:21:41 +0100

    feat(run): run last position
    See #4

--- lua/neotest/consumers/run.lua
+++ lua/neotest/consumers/run.lua
@@ -3,15 +3,22 @@ local lib = require("neotest.lib")
 
 ---@param client neotest.Client
 local function init(client)
-  local function get_tree_from_args(args)
-    if args[1] then
-      local position_id = lib.files.exists(args[1]) and async.fn.fnamemodify(args[1], ":p")
-        or args[1]
-      return client:get_position(position_id, args)
+  local last_run
+  local function get_tree_from_args(args, store)
+    local tree, adapter = (function()
+      if args[1] then
+        local position_id = lib.files.exists(args[1]) and async.fn.fnamemodify(args[1], ":p")
+            or args[1]
+        return client:get_position(position_id, args)
+      end
+      local file_path = async.fn.expand("%:p")
+      local row = async.fn.getpos(".")[2] - 1
+      return client:get_nearest(file_path, row, args)
+    end)()
+    if tree and store then
+      last_run = { tree:data().id, vim.tbl_extend("keep", args, { adapter = adapter }) }
     end
-    local file_path = async.fn.expand("%:p")
-    local row = async.fn.getpos(".")[2] - 1
-    return client:get_nearest(file_path, row, args)
+    return tree
   end
 
   return {
@@ -21,9 +28,26 @@ local function init(client)
         args = { args }
       end
       async.run(function()
-        local tree = get_tree_from_args(args)
+        local tree = get_tree_from_args(args, true)
         if not tree then
           lib.notify("No tests found")
+          return
+        end
+        client:run_tree(tree, args)
+      end)
+    end,
+    run_last = function(args)
+      args = args or {}
+      if not last_run then
+        lib.notify("No tests run yet")
+        return
+      end
+      async.run(function()
+        local position_id, last_args = unpack(last_run)
+        args = vim.tbl_extend("keep", args, last_args)
+        local tree = client:get_position(position_id, args)
+        if not tree then
+          lib.notify("Last test run no longer exists")
           return
         end
         client:run_tree(tree, args)
@@ -92,6 +116,26 @@ neotest.run = {}
 ---@field strategy string | neotest.Strategy: Strategy to run commands with
 ---@field extra_args string[]: Extra arguments for test command
 function neotest.run.run(args) end
+
+---Re-run the last position that was run.
+---Arguments are optional
+---
+---Run the last position that was run with the same arguments and strategy
+---<pre>
+--->
+---lua require("neotest").run.run_last()
+---</pre>
+---
+---Run the last position that was run with the same arguments but debug with nvim-dap
+---<pre>
+--->
+---lua require("neotest").run.run_last({ strategy = "dap" })
+---</pre>
+---@param args table: Arguments to run with
+---@field adapter string: Adapter ID, if not given the same adapter as the last run is used.
+---@field strategy string | neotest.Strategy: Strategy to run commands with
+---@field extra_args string[]: Extra arguments for test command
+function neotest.run.run_last(args) end
 
 ---Stop a running process
 ---@param args string | table: Position ID to stop or args. If args then args[1] should be the position ID.
