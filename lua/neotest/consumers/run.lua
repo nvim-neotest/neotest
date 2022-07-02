c57dc67383900084df12148398428e6462f48023
Rónán Carrigan <rcarriga@tcd.ie>
2022-07-02 13:51:53 +0100

    feat(run): run suite

--- lua/neotest/consumers/run.lua
+++ lua/neotest/consumers/run.lua
@@ -1,87 +1,27 @@
 local async = require("neotest.async")
 local lib = require("neotest.lib")
 
----@param client neotest.Client
-local function init(client)
-  local last_run
-  local function get_tree_from_args(args, store)
-    local tree, adapter = (function()
-      if args[1] then
-        local position_id = lib.files.exists(args[1]) and async.fn.fnamemodify(args[1], ":p")
-          or args[1]
-        return client:get_position(position_id, args)
-      end
-      local file_path = async.fn.expand("%:p")
-      local row = async.fn.getpos(".")[2] - 1
-      return client:get_nearest(file_path, row, args)
-    end)()
-    if tree and store then
-      last_run = { tree:data().id, vim.tbl_extend("keep", args, { adapter = adapter }) }
+---@type  neotest.Client
+local client
+local last_run
+local function get_tree_from_args(args, store)
+  local tree, adapter = (function()
+    if args.suite then
+      return client:get_position(nil, args)
     end
-    return tree
+    if args[1] then
+      local position_id = lib.files.exists(args[1]) and async.fn.fnamemodify(args[1], ":p")
+          or args[1]
+      return client:get_position(position_id, args)
+    end
+    local file_path = async.fn.expand("%:p")
+    local row = async.fn.getpos(".")[2] - 1
+    return client:get_nearest(file_path, row, args)
+  end)()
+  if tree and store then
+    last_run = { tree:data().id, vim.tbl_extend("keep", args, { adapter = adapter }) }
   end
-
-  return {
-    run = function(args)
-      args = args or {}
-      if type(args) == "string" then
-        args = { args }
-      end
-      async.run(function()
-        local tree = get_tree_from_args(args, true)
-        if not tree then
-          lib.notify("No tests found")
-          return
-        end
-        client:run_tree(tree, args)
-      end)
-    end,
-    run_last = function(args)
-      args = args or {}
-      if not last_run then
-        lib.notify("No tests run yet")
-        return
-      end
-      async.run(function()
-        local position_id, last_args = unpack(last_run)
-        args = vim.tbl_extend("keep", args, last_args)
-        local tree = client:get_position(position_id, args)
-        if not tree then
-          lib.notify("Last test run no longer exists")
-          return
-        end
-        client:run_tree(tree, args)
-      end)
-    end,
-    attach = function(args)
-      args = args or {}
-      if type(args) == "string" then
-        args = { args }
-      end
-      async.run(function()
-        local pos = get_tree_from_args(args)
-        if not pos then
-          lib.notify("No tests found in file", "warn")
-          return
-        end
-        client:attach(pos, args)
-      end)
-    end,
-    stop = function(args)
-      args = args or {}
-      if type(args) == "string" then
-        args = { args }
-      end
-      async.run(function()
-        local tree = get_tree_from_args(args)
-        if not tree then
-          lib.notify("No tests found", "warn")
-          return
-        end
-        client:stop(tree, args)
-      end)
-    end,
-  }
+  return tree
 end
 
 ---@tag neotest.run
@@ -112,11 +52,25 @@ neotest.run = {}
 ---lua require("neotest").run.run({vim.fn.expand("%"), strategy = "dap"})
 ---</pre>
 ---@param args string | table: Position ID to run or args. If args then args[1] should be the position ID.
+---@field suite boolean: Run the entire suite instead of a single position
 ---@field adapter string: Adapter ID, if not given the first adapter found with chosen position is used.
 ---@field strategy string | neotest.Strategy: Strategy to run commands with
 ---@field extra_args string[]: Extra arguments for test command
 ---@field env table<string, string>: Extra environment variables to add to the environment of tests
-function neotest.run.run(args) end
+function neotest.run.run(args)
+  args = args or {}
+  if type(args) == "string" then
+    args = { args }
+  end
+  async.run(function()
+    local tree = get_tree_from_args(args, true)
+    if not tree then
+      lib.notify("No tests found")
+      return
+    end
+    client:run_tree(tree, args)
+  end)
+end
 
 ---Re-run the last position that was run.
 ---Arguments are optional
@@ -137,21 +91,64 @@ function neotest.run.run(args) end
 ---@field strategy string | neotest.Strategy: Strategy to run commands with
 ---@field extra_args string[]: Extra arguments for test command
 ---@field env table<string, string>: Extra environment variables to add to the environment of tests
-function neotest.run.run_last(args) end
+function neotest.run.run_last(args)
+  args = args or {}
+  if not last_run then
+    lib.notify("No tests run yet")
+    return
+  end
+  async.run(function()
+    local position_id, last_args = unpack(last_run)
+    args = vim.tbl_extend("keep", args, last_args)
+    local tree = client:get_position(position_id, args)
+    if not tree then
+      lib.notify("Last test run no longer exists")
+      return
+    end
+    client:run_tree(tree, args)
+  end)
+end
 
 ---Stop a running process
 ---@param args string | table: Position ID to stop or args. If args then args[1] should be the position ID.
 ---@field adapter string: Adapter ID, if not given the first adapter found with chosen position is used.
-function neotest.run.stop(args) end
+function neotest.run.stop(args)
+  args = args or {}
+  if type(args) == "string" then
+    args = { args }
+  end
+  async.run(function()
+    local tree = get_tree_from_args(args)
+    if not tree then
+      lib.notify("No tests found", "warn")
+      return
+    end
+    client:stop(tree, args)
+  end)
+end
 
 ---Attach to a running process for the given position.
 ---@param args string | table: Position ID to attach to or args. If args then args[1] should be the position ID.
 ---@field adapter string: Adapter ID, if not given the first adapter found with chosen position is used.
-function neotest.run.attach(args) end
+function neotest.run.attach(args)
+  args = args or {}
+  if type(args) == "string" then
+    args = { args }
+  end
+  async.run(function()
+    local pos = get_tree_from_args(args)
+    if not pos then
+      lib.notify("No tests found in file", "warn")
+      return
+    end
+    client:attach(pos, args)
+  end)
+end
 
 neotest.run = setmetatable(neotest.run, {
-  __call = function(_, ...)
-    return init(...)
+  __call = function(_, client_)
+    client = client_
+    return neotest.run
   end,
 })
 
