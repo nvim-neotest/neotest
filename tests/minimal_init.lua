02104a070a9af78934d0879e068ea3abaaaad984
Rónán Carrigan <ronan@calypsoai.com>
2023-10-20 14:39:09 +0100

    ci: fix tests

--- tests/minimal_init.lua
+++ tests/minimal_init.lua
@@ -3,6 +3,7 @@ vim.notify = print
 vim.opt.rtp:append(".")
 vim.opt.rtp:append(lazypath .. "/plenary.nvim")
 vim.opt.rtp:append(lazypath .. "/nvim-dap")
+vim.opt.rtp:append(lazypath .. "/nvim-treesitter")
 vim.opt.swapfile = false
 A = function(...)
   print(vim.inspect(...))
