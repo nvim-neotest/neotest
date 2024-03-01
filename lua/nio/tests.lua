b6e24b7093f8baaec27d90dd7c5d81d76d568fe4
HiPhish <hiphish@posteo.de>
2024-03-01 01:13:07 +0100

    Add async context function to tests module
    As a user of the nio library when writing tests I might want to run an
    asynchronous function within a synchronous function and wait for the
    result.  This function acts as a synchronous wrapper around the
    asynchronous function.

--- lua/nio/tests.lua
+++ lua/nio/tests.lua
@@ -8,12 +8,14 @@ local nio = {}
 nio.tests = {}
 
 local with_timeout = function(func, timeout)
-  local success, err
+  local success, err, results
   return function()
-    local task = tasks.run(func, function(success_, err_)
+    local task = tasks.run(func, function(success_, ...)
       success = success_
       if not success_ then
-        err = err_
+        err = ...
+      else
+        results = { ... }
       end
     end)
 
@@ -26,6 +28,7 @@ local with_timeout = function(func, timeout)
     elseif not success then
       error(string.format("Test task failed with message:\n%s", err))
     end
+    return unpack(results)
   end
 end
 
@@ -43,6 +46,21 @@ end
 ---@param async_func function
 nio.tests.after_each = function(async_func)
   after_each(with_timeout(async_func))
+end
+
+---Run the given function, applied to the remaining arguments, in an
+---asynchronous context.  The return value (or values) is the return value of
+---the asynchronous function.
+---@param async_func function  Function to execute
+---@param ... any  Arguments to `async_func`
+---@return any ...  Return values of `async_func`
+nio.tests.with_async_context = function(async_func, ...)
+  local args = { ... }
+  local thunk = function()
+    return async_func(unpack(args))
+  end
+  local result = with_timeout(thunk, tonumber(vim.env.PLENARY_TEST_TIMEOUT))()
+  return result
 end
 
 return nio.tests
