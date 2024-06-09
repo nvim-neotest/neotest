02eb25608d40134444c67a6ededbf247972a606c
rcarriga <rcarriga@tcd.ie>
2024-06-09 17:37:16 +0100

    fix(utils): use nio.fn
    See #410

--- lua/neotest/utils/init.lua
+++ lua/neotest/utils/init.lua
@@ -1,7 +1,9 @@
+local nio = require("nio")
+
 local M = {}
 
 function M.tbl_flatten(t)
-  return vim.fn.has("nvim-0.11") == 1 and vim.iter(t):flatten(math.huge):totable()
+  return nio.fn.has("nvim-0.11") == 1 and vim.iter(t):flatten(math.huge):totable()
     or vim.tbl_flatten(t)
 end
 
