63083a9bf2bb382136eff1643000cbceae52a851
xckomorebi <53259250+xckomorebi@users.noreply.github.com>
2026-03-23 16:17:34 +0800

    fix(utils): use vim.fn.has instead of nio.fn.has in tbl_flatten (#579)

--- lua/neotest/utils/init.lua
+++ lua/neotest/utils/init.lua
@@ -1,9 +1,7 @@
-local nio = require("nio")
-
 local M = {}
 
 function M.tbl_flatten(t)
-  return nio.fn.has("nvim-0.11") == 1 and vim.iter(t):flatten(math.huge):totable()
+  return vim.fn.has("nvim-0.11") == 1 and vim.iter(t):flatten(math.huge):totable()
     or vim.tbl_flatten(t)
 end
 
