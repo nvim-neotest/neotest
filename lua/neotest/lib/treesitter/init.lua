2ffca3aefb070e31f1ac00e9fbfd1a83f995c326
Liam Cooper <liamcoop@outlook.com>
2026-03-28 22:02:55 -0700

    Apply fix by @nsidorenco in Nsidorenco to neotest project, took a look through tests. Saw and un-disabled tests/unit/lib/treesitter/init_spec_disabled.lua and made sure things were still passing after this change

--- lua/neotest/lib/treesitter/init.lua
+++ lua/neotest/lib/treesitter/init.lua
@@ -56,11 +56,12 @@ local function collect(file_path, query, source, root, opts)
       range = { root:range() },
     },
   }
-  for _, match, metadata in query:iter_matches(root, source, nil, nil, { all = false }) do
+  for _, match, metadata in query:iter_matches(root, source, nil, nil) do
     local captured_nodes = {}
     local node_metadata = {}
     for i, capture in ipairs(query.captures) do
-      captured_nodes[capture] = match[i]
+      local nodes = match[i]
+      captured_nodes[capture] = type(nodes) == "table" and nodes[#nodes] or nodes
       node_metadata[capture] = metadata[i]
     end
     local res = opts.build_position(file_path, source, captured_nodes, node_metadata)
